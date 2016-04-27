16f884b38508258abf28f1308e9a803164db028c
Donew-Mac <duanpeijian@outlook.com>
2016-04-27 12:33:12 +0800

    Initial Commit

---
+++ UnityPurchasing/UnityPurchasing.h
@@ -0,0 +1,13 @@
+//
+//  UnityPurchasing.h
+//  UnityPurchasing
+//
+//  Created by Donew on 16/4/27.
+//  Copyright © 2016年 Donew. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UnityPurchasing : NSObject
+
+@end
---
+++ UnityPurchasing/UnityPurchasing.m
@@ -0,0 +1,13 @@
+//
+//  UnityPurchasing.m
+//  UnityPurchasing
+//
+//  Created by Donew on 16/4/27.
+//  Copyright © 2016年 Donew. All rights reserved.
+//
+
+#import "UnityPurchasing.h"
+
+@implementation UnityPurchasing
+
+@end
