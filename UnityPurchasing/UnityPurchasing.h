7a84227773eaed895c00314982c56b253bc13c1f
Donew-Mac <duanpeijian@outlook.com>
2016-04-27 12:40:27 +0800

    UnityPurchasing初始化

--- UnityPurchasing/UnityPurchasing.h
+++ UnityPurchasing/UnityPurchasing.h
@@ -1,13 +1,38 @@
-//
-//  UnityPurchasing.h
-//  UnityPurchasing
-//
-//  Created by Donew on 16/4/27.
-//  Copyright © 2016年 Donew. All rights reserved.
-//
+#import <StoreKit/StoreKit.h>
 
-#import <Foundation/Foundation.h>
+// Callback to Unity identifying the subject, JSON message body and optional app receipt.
+// Note that App Receipts are sent separately to the JSON body for performance reasons.
+typedef void (*UnityPurchasingCallback)(const char* subject, const char* payload, const char* receipt, const char* transactionId);
 
-@interface UnityPurchasing : NSObject
+@interface ProductDefinition : NSObject
+    
+@property (nonatomic, strong) NSString *id;
+@property (nonatomic, strong) NSString *storeSpecificId;
+@property (nonatomic, strong) NSString *type;
+@end
+
+@interface ReceiptRefresher : NSObject <SKRequestDelegate>
+
+@property (nonatomic, strong) void (^callback)(BOOL);
+
+@end
+
+@interface UnityPurchasing : NSObject <SKProductsRequestDelegate, SKPaymentTransactionObserver> {
+    UnityPurchasingCallback messageCallback;
+    NSMutableDictionary* validProducts;
+    NSSet* productIds;
+    SKProductsRequest *request;
+    NSMutableDictionary *pendingTransactions;
+}
+
++ (NSArray*) deserializeProductDefs:(NSString*)json;
++ (ProductDefinition*) deserializeProductDef:(NSString*)json;
++ (NSString*) serializeProductMetadata:(NSArray*)products;
+
+-(void) restorePurchases;
+-(NSString*) getAppReceipt;
+-(void) addTransactionObserver;
+@property (nonatomic, strong) ReceiptRefresher* receiptRefresher;
+@property (nonatomic, strong) SKReceiptRefreshRequest* refreshRequest;
 
 @end
